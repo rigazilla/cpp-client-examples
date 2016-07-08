20bc3d4279e21414c74b3e1088a367dfda4d6cee
Vittorio Rigamonti <Vittorio Rigamonti>
2016-07-09 00:38:52 +0200

    changes in example

--- query/queryExample.cpp
+++ query/queryExample.cpp
@@ -1,10 +1,3 @@
-/*
- * QueryTest.cpp
- *
- *  Created on: Apr 6, 2016
- *      Author: rigazilla
- */
-
 #include "addressbook.pb.h"
 #include "bank.pb.h"
 #include <infinispan/hotrod/BasicTypesProtoStreamMarshaller.h>
@@ -32,10 +25,7 @@ std::string read(std::string file)
 	return buffer.str();
 }
 
-
 int main(int argc, char** argv) {
-
-    auto result=0;
 	std::cout << "Tests for Query" << std::endl;
     ConfigurationBuilder builder;
     builder.addServer().host(argc > 1 ? argv[1] : "127.0.0.1").port(argc > 2 ? atoi(argv[2]) : 11222).protocolVersion(Configuration::PROTOCOL_VERSION_24);
@@ -45,7 +35,6 @@ int main(int argc, char** argv) {
     //Create the Protobuf Metadata cache peer with a Protobuf marshaller
     auto *km = new BasicTypesProtoStreamMarshaller<std::string>();
     auto *vm = new BasicTypesProtoStreamMarshaller<std::string>();
-
     auto metadataCache = cacheManager.getCache<std::string, std::string>(
     		km, &Marshaller<std::string>::destroy, vm, &Marshaller<std::string>::destroy,PROTOBUF_METADATA_CACHE_NAME, false);
     // Install the data model into the Protobuf metadata cache
@@ -53,16 +42,15 @@ int main(int argc, char** argv) {
     if (metadataCache.containsKey(ERRORS_KEY_SUFFIX))
 	{
       std::cerr << "fail: error in registering .proto model" << std::endl;
-      result=-1;
-      return result;
+      return -1;
 	}
-    //Create the user cache peer with a Protobuf marshaller
+    //Create the application cache peer with a Protobuf marshaller
     auto *testkm = new BasicTypesProtoStreamMarshaller<int>();
     auto *testvm = new ProtoStreamMarshaller<sample_bank_account::User>();
     auto testCache = cacheManager.getCache<int, sample_bank_account::User>(
     		testkm, &Marshaller<int>::destroy, testvm, &Marshaller<sample_bank_account::User>::destroy, "namedCache", false);
 
-    //Fill the cache with user data: two users Tom and Jerry
+    //Fill the cache with the application data: two users Tom and Jerry
     testCache.clear();
     sample_bank_account::User_Address a;
     sample_bank_account::User user1;
@@ -78,6 +66,7 @@ int main(int argc, char** argv) {
     user1.set_id(4);
     user1.set_name("Jerry");
     user1.set_surname("Mouse");
+    addr->set_street("Via Milano");
     user1.set_gender(sample_bank_account::User_Gender_MALE);
     testCache.put(4, user1);
     // Simple query to get User objects
@@ -93,6 +82,19 @@ int main(int argc, char** argv) {
 					<< ",surname=" << res[i].surname() << ")" << std::endl;
 		}
 	}
+    // Simple query to get User objects with where condition
+	{
+		QueryRequest qr;
+		std::cout << "from sample_bank_account.User u where u.addresses.street=\"Via Milano\"" << std::endl;
+		qr.set_jpqlstring("from sample_bank_account.User u where u.addresses.street=\"Via Milano\"");
+		QueryResponse resp = testCache.query(qr);
+		std::vector<sample_bank_account::User> res;
+		unwrapResults(resp, res);
+		for (auto i = 0; i < res.size(); i++) {
+			std::cout << "User(id=" << res[i].id() << ",name=" << res[i].name()
+					<< ",surname=" << res[i].surname() << ")" << std::endl;
+		}
+	}
 	// Simple query to get projection (name, surname)
 	{
 		QueryRequest qr;
@@ -100,50 +102,23 @@ int main(int argc, char** argv) {
 		qr.set_jpqlstring(
 				"select u.name, u.surname from sample_bank_account.User u");
 		QueryResponse resp = testCache.query(qr);
-
+        //Typed resultset
 		std::vector<std::tuple<std::string, std::string> > prjRes;
 
-		if (!unwrapProjection(resp, prjRes)) {
-			std::cerr << "fail: projection not found" << std::endl;
-			result = -1;
-			return result;
-		}
-
+		unwrapProjection(resp, prjRes);
 		for (auto i = 0; i < prjRes.size(); i++) {
-			std::cout << std::get < 0 > (prjRes[i]) << "  " << std::get < 1
-					> (prjRes[i]) << "  " << std::endl;
+			std::cout << "Name: " << std::get<0> (prjRes[i])
+							  << " Surname: " << std::get<1> (prjRes[i]) << std::endl;
 		}
 	}
-//	{
-//		QueryRequest qr;
-//		qr.set_jpqlstring(
-//				"select u.surname, u.id from sample_bank_account.User u");
-//		QueryResponse resp = testCache.query(qr);
-//		std::vector<std::tuple<std::string, int> > prjRes;
-//
-//		if (!unwrapProjection(resp, prjRes)) {
-//			std::cerr << "fail: projection not found" << std::endl;
-//			result = -1;
-//			return result;
-//		}
-//
-//		for (unsigned int i = 0; i < prjRes.size(); i++) {
-//			std::cout << std::get < 0 > (prjRes[i]) << "  " << std::get < 1
-//					> (prjRes[i]) << "  " << std::endl;
-//		}
-//	}
-//	{
-//		QueryRequest qr;
-//		qr.set_jpqlstring(
-//				"select count(u.surname) from sample_bank_account.User u");
-//		QueryResponse resp = testCache.query(qr);
-//
-//		int i = unwrapSingleResult<int>(resp);
-//
-//			std::cout << "result is: " << i << std::endl;
-//	}
-
-
-	return result;
+	{
+		QueryRequest qr;
+		qr.set_jpqlstring("select count(u.id) from sample_bank_account.User u");
+		QueryResponse resp = testCache.query(qr);
+		int i = unwrapSingleResult<int>(resp);
+		std::cout << "Result is: " << i << std::endl;
+	}
+	cacheManager.stop();
+	return 0;
 }
 
